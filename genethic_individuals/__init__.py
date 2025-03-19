a3966e187d384106c054b9074566a65c40eaee3a
lucianobizin <lucianobizin@gmail.com>
2025-03-19 21:57:28 +0100

    subiendo nueva estructura

--- genethic_individuals/__init__.py
+++ genethic_individuals/__init__.py
@@ -8,7 +8,6 @@ class Individual:
     def __init__(self, bounds_dict: Dict[str, Tuple[Union[int, float]]], properties: dict, generation: int):
         """
         Clase que va a instanciar los distintos individuos que van a competir.
-        :param qm_conn_obj: Objeto de conexión con el ordenador o el simulador cuántico
         :param bounds_dict: Diccionario en el que se definen los parámetros a optimizar y sus valores, ej. '{learning_rate: (0.0001, 0.1)}'
         de uso y que desemboca en un individuo que se deshechará por tener una malformación. Por ejemplo, si estamos optimizando un learning_rate y la mutación nos da un valor
         superior a 1, ese individuo, se descarta antes de ser evaluado. ej. '{learning_rate: (0.000001, 1)}', si los supera, consideramos malformación.
